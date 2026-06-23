7e953f6d7f0f08e7f73689c23a43b225e083d5bd
土豆 <787303657@qq.com>
2026-06-23 23:04:50 +0800

    Add files via upload

---
+++ 自学第一天.py
@@ -0,0 +1,30 @@
+###变量名赋值
+name = "陈三"
+age = 22
+
+name1 = "小明"
+age1 = 18
+
+name2 = "小红"
+age2 = 18
+
+name3 = "小强"
+age3 = 18
+
+name4 = "小美"
+age4 = 18
+
+name5 = "小六"
+age5 = 18
+###输出内容
+print(f"我的名字叫{name},我今年{age}岁了。")
+
+print(f"我的名字叫{name1}，我今年{age1}岁了。")
+
+print(f"我叫{name2},今年{age2}岁了。")
+
+print(f"我叫{name3},今年{age3}岁了。")
+
+print(f"我叫{name4},今年{age4}岁了。")
+
+print(f"我叫{name5},今年{age5}岁了。")
